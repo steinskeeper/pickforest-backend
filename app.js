58f6bedecbef7a39ce469f2df044bd8d0295df63
steinskeeper <dpelpsykongroo@gmail.com>
2022-03-08 10:59:25 +0530

    no server

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var whitelist = [
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
+      if (whitelist.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
         callback(new Error("Dont waste tour time"));
