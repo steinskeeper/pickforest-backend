74c97be10b516294457b1420067ccd7faf55ea1f
steinskeeper <dpelpsykongroo@gmail.com>
2022-03-08 11:00:34 +0530

    cors again

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var whitelist = [
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (whitelist.indexOf(origin) !== -1) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
         callback(null, true);
       } else {
         callback(new Error("Dont waste tour time"));
